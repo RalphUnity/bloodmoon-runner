a1eee3e8366280c6b48d3a1ee397c4398e12b856
RalphUnity <55875029+RalphUnity@users.noreply.github.com>
2019-09-27 19:27:43 +0800

    first commit
    Uploaded the first existing project

---
+++ Endless runner/Assets/Scripts/DeathMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu : MonoBehaviour {
+
+	public string mainMenuLevel;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+	public void RestartGame(){
+        Time.timeScale = 1f;
+        FindObjectOfType<GameManager>().Reset();
+	}
+
+	public void QuitToMain(){
+		SceneManager.LoadScene(mainMenuLevel);
+	}
+}
---
+++ Endless runner/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float scrollSpeed;
+    private Vector2 savedOffset;
+
+    void Start () {
+        savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
+    }
+
+    void Update () {
+        float y = Mathf.Repeat (Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2 (y, savedOffset.x);
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+    }
+
+    public void OnDisable () {
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+    }
+
+
+    /*public void Stop(){
+        scrollSpeed = 0f;   Stop Scrolling
+    }*/
+}
